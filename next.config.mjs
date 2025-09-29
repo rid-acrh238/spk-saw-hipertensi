ae9edcb811f642115a5860ec4bc44db1dd5dc053
Kazzpars <achmadridwan587@gmail.com>
2025-09-29 16:49:33 +0700

    ready to production

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,14 @@
-const repoBase = process.env.NEXT_PUBLIC_BASE_PATH || 'spk-saw-hipertensi'
+const repoBase = process.env.NEXT_PUBLIC_BASE_PATH || '/spk-saw-hipertensi';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   output: 'export',
-  basePath: repoBase || undefined,
-  assetPrefix: repoBase ? `${repoBase}/` : undefined,
+  basePath: repoBase,
+  assetPrefix: `${repoBase}/`,
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
